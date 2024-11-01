ce7f9fe33ef2af44d4a2fad66dfa763effd8e8bf
belaletech <belalahmad@lambdatest.com>
2024-11-01 17:43:48 +0530

    upload

--- src/test/java/com/lambdatest/tunnel/LambdaTestTunnelTest.java
+++ src/test/java/com/lambdatest/tunnel/LambdaTestTunnelTest.java
@@ -17,9 +17,11 @@ public class LambdaTestTunnelTest {
 
 	WebDriver driver = null;
 	public static String status = "passed";
+	String username="belalahmad";
+	String access_key="sZhRHD9cmkV1nXNWEUw7psGAxRgN84ugdKqGMforKfXJCQhIU7";
 
-	String username = System.getenv("LT_USERNAME");
-	String access_key = System.getenv("LT_ACCESS_KEY");
+//	String username = System.getenv("LT_USERNAME");
+//	String access_key = System.getenv("LT_ACCESS_KEY");
 
 	@BeforeTest
 	public void setUp() throws Exception {
@@ -31,6 +33,7 @@ public class LambdaTestTunnelTest {
 		capabilities.setCapability("browserName", "Chrome");
 		capabilities.setCapability("version","latest");
 		capabilities.setCapability("tunnel",true);
+		capabilities.setCapability("tunnelName","belaltunnel");
 		capabilities.setCapability("network",true);
 		capabilities.setCapability("console",true);
 		capabilities.setCapability("visual",true);
@@ -40,6 +43,7 @@ public class LambdaTestTunnelTest {
 		HashMap<String, String> options = new HashMap<String, String>();
 		options.put("user", username);
 		options.put("key", access_key);
+		options.put("tunnelName","belaltunnel");
 
 		//start tunnel
 		t.start(options);
@@ -50,9 +54,14 @@ public class LambdaTestTunnelTest {
 	@Test()
 	public void testTunnel() throws Exception {
 		//Check LocalHost on XAMPP
+		System.out.println("Navigate");
 		driver.get("http://localhost.lambdatest.com");
+		System.out.println("visit site");
+		Thread.sleep(2000);
 		// Let's check that the item we added is added in the list.
 		driver.get("https://google.com");
+		System.out.println("visiteaad");
+		Thread.sleep(2000);
 	}
 
 	@AfterTest
